dca153d9c02f9bd196a0ef126edffd3d499f623f
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-09-27 11:31:23 +0300

    actiontype

---
+++ src/AddItem.tsx
@@ -0,0 +1,51 @@
+import {Button, IconButton, TextField} from "@material-ui/core";
+import { ControlPoint } from "@material-ui/icons";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+
+type AddItemType = {
+    addItem: (title: string) => void
+}
+
+export function AddItem(props: AddItemType) {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== "") {
+            props.addItem(newTitle);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addTask();
+        }
+    }
+
+
+    return <div>
+        <TextField value={title}
+                   variant={'outlined'}
+                   label={'Type value'}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   error={!!error}
+                   helperText={error}
+        />
+        <IconButton onClick={addTask}  color={'primary'}>
+            <ControlPoint />
+        </IconButton>
+
+    </div>
+}
